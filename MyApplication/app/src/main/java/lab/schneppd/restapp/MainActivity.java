50942167f1701bb51c3b25bbf4e86f8004d98550
david.schnepp <david.schnepp@schneppd.com>
2017-06-22 10:12:16 +0200

    add external libs

--- MyApplication/app/src/main/java/lab/schneppd/restapp/MainActivity.java
+++ MyApplication/app/src/main/java/lab/schneppd/restapp/MainActivity.java
@@ -2,6 +2,7 @@ package lab.schneppd.restapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvDebug = (TextView) findViewById(R.id.txtDebug);
+        tvDebug.setText("start of debug");
     }
 }
